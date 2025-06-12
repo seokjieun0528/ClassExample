3ede58f0a8e0918334790d1e7b9f621781ee0ccb
Another Name <s2307@e-mirim.hs.kr>
2025-06-12 14:12:58 +0900

    상속 관계에서의 클래스 변수 예

--- Program.cs
+++ Program.cs
@@ -8,8 +8,15 @@ namespace ClassExample
 {
     class Program
     {
+        
         class Parent
         {
+            public static int counter = 0;
+            public void CountParent()
+            {
+                Parent.counter++;
+            }
+
             public Parent() { Console.WriteLine("Parent()"); }
             public Parent(int param) { Console.WriteLine("Parent(int param)"); }
             public Parent(string param) { Console.WriteLine("Parent(string param)"); }
@@ -17,6 +24,11 @@ namespace ClassExample
 
         class Child : Parent
         {
+            public void CountChild()
+            {
+                Child.counter++;
+            }
+            
             public Child() : base(10)
             {
                 Console.WriteLine("Chile():base(10)");
@@ -32,6 +44,12 @@ namespace ClassExample
             Child childA = new Child();
             Child childB = new Child("string");
 
+            Parent parent = new Parent();
+            Child child = new Child();
+            parent.CountParent();
+            child.CountChild();
+            Console.WriteLine(Parent.counter);
+            Console.WriteLine(Child.counter);
         }
     }
 }
