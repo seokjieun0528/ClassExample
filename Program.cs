f40a3ff6c67509ea687257130c4a94bd0ca966ab
Another Name <s2307@e-mirim.hs.kr>
2025-06-12 14:06:16 +0900

    상속 시 생성자 예

--- Program.cs
+++ Program.cs
@@ -8,8 +8,30 @@ namespace ClassExample
 {
     class Program
     {
+        class Parent
+        {
+            public Parent() { Console.WriteLine("Parent()"); }
+            public Parent(int param) { Console.WriteLine("Parent(int param)"); }
+            public Parent(string param) { Console.WriteLine("Parent(string param)"); }
+        }
+
+        class Child : Parent
+        {
+            public Child() : base(10)
+            {
+                Console.WriteLine("Chile():base(10)");
+            }
+            public Child(string input) : base(input)
+            {
+                Console.WriteLine("Child(string input) : base(input)");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Child childA = new Child();
+            Child childB = new Child("string");
+
         }
     }
 }
